22e7e99e301bd155745474e1471da236fc788773
missDance <anna@avenberg.se>
2020-04-08 18:15:30 +0200

    added a link

--- test_tweets_5_languages/model_2.py
+++ test_tweets_5_languages/model_2.py
@@ -1,2 +1,3 @@
 # link to text about n-gram model in python
 # https://stackabuse.com/python-for-nlp-developing-an-automatic-text-filler-using-n-grams/
+# https://stackoverflow.com/questions/18658106/quick-implementation-of-character-n-grams-for-word
